5c22cdebf6874e38fbf0249010186e26101ec6a8
Rodrigo Oliveira <rodrigo.silvoliveira@gmail.com>
2021-09-10 18:03:42 -0300

    Update run.sh

--- Códigos/run.sh
+++
@@ -1,3 +0,0 @@
-g++ -o ACO ACO.cpp
-./ACO 100 200 0 1 0.3 1.0 "../Instâncias/M_20x20.txt"
-python3 PlotACO.py
---
+++ run.sh
@@ -0,0 +1,3 @@
+g++ -o Códigos/ACO Códigos/ACO.cpp
+./Códigos/ACO 100 200 0 1 0.3 1.0 "Instâncias/M_20x20.txt"
+python3 Códigos/PlotACO.py
